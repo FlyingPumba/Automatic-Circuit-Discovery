Fix gradient calculation (#30)
* Calculate ablation cache with gradients by default

Why?

We want to do gradient descent both on the input and on the patch input. This means that we need to get gradients on the patch input. The patch input only influences the output through the ablation cache, so we should calculate gradients when calculating the ablation cache.

In places where we do not need the gradient, I've added back `with torch.no_grad()` for performance reasons.

* Calculate gradient in ablation cache by using own hooks

The `run_with_cache` method from TransformerLens uses hooks that store detached tensors in the cache. As a result, you can't calculate any gradients with the cache.

This commit introduces a small adapted set of hooks that stores the attached tensors in the cache.

* Fix backward pass by disabling pytorch.utils.checkpoint

acdc/greaterthan/utils.py
@@ -255,7 +255,7 @@ def get_year_data(num_examples, model):
     return prompts_tokenized, prompts
 
 
-def get_all_greaterthan_things(num_examples, metric_name, device="cuda"):
+def get_all_greaterthan_things(num_examples, metric_name, device: str | torch.device = "cuda"):
     model = get_gpt2_small(device=device)
     data, prompts = get_year_data(num_examples * 2, model)
     patch_data = data.clone()